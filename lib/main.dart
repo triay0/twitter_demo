8dc58fa645d124ff4026b2fe8dae2ca86d20df28
Dani <danitriay@outlook.com>
2019-05-22 10:16:50 +0200

    Static timeline FAB create tweet

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme:ThemeData.light(),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -27,16 +16,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,7 +23,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   int _selectedIndex = 0;
 
   static const TextStyle optionStyle =
@@ -102,9 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: _titles.elementAt(_selectedIndex),
-      ),
+      floatingActionButton: FloatingActionButton(onPressed: null,child: Icon( Icons.edit),),
       drawer: Drawer(
           child: ListView(
         children: <Widget>[
@@ -139,6 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       )),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -157,30 +134,38 @@ class _MyHomePageState extends State<MyHomePage> {
             title: Text('Messages'),
           ),
         ],
-        unselectedItemColor: Colors.black12,
+        unselectedItemColor: Colors.black45,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         selectedItemColor: Colors.blue,
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
-      body: Center(
-          child: ListView.separated(
-              separatorBuilder: (context, index) => Divider(
-                    color: Colors.blueGrey,
-                  ),
-              itemCount: 15,
-              itemBuilder: (BuildContext ctxt, int index) {
-                return new ListTile(
-                  trailing: Text('1h'),
-                  leading: Icon(Icons.account_circle),
-                  title: Text('Helow'),
-                  subtitle: Text(
-                      'asdfgjhkhg asdfgjhkhg asdfgjhkhgasdfgjhkhgasdfgjhkhg asdfgjhkhg asdfgjhkhg  '),
-                );
-              })
-          //_content.elementAt(_selectedIndex),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: _titles.elementAt(_selectedIndex),
+            floating: true,
           ),
+          SliverList(delegate: SliverChildListDelegate(timelineBuilder()))
+        ],
+      ),
     );
+  }
+
+  List timelineBuilder() {
+    List<Widget> tweets = List();
+    for (int i = 0; i < 50; i++) {
+      Widget tweet = new ListTile(
+        trailing: Text('1h'),
+        leading: Icon(Icons.account_circle),
+        title: Text('Helow'),
+        subtitle: Text(
+            'asdfgjhkhg asdfgjhkhg asdfgjhkhgasdfgjhkhgasdfgjhkhg asdfgjhkhg asdfgjhkhg  '),
+      );
+
+      tweets.add(tweet);
+    }
+    return tweets;
   }
 }
